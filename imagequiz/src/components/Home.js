18defbaf0274d0c85f22f68ffc771d8b513e29fd
luclemaster <llemaster@email.arizona.edu>
2020-10-08 11:24:20 -0700

    added more complex login

--- imagequiz/src/components/Home.js
+++ imagequiz/src/components/Home.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state ={
-            showLoginForm: false
+            username: '',
+            showLoginForm: false,
+            authenticated: false
         };
     }
     login = () => {
@@ -12,24 +15,41 @@ class Home extends React.Component {
             showLoginForm: true
         });
     }
+
+    onSubmit = (event) => {
+        if(this.state.username.trim().length > 0)
+        {
+            this.setState({authenticated: true});
+        }
+        event.preventDefault();
+    }
+
+    handleChange = (event) => {
+        
+    }
     render() {
-        if (this.state.showLoginForm)
+        if ((!this.state.authenticated) && this.state.showLoginForm)
         {
             return(
                 <div>
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <lable>Username:</lable>
-                        <input type ="text"></input>
+                        <input 
+                        type ="text" 
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                        ></input>
                         <button type="submit">Login</button>
                     </form>
                 </div>
-
             );
         }
         return (
             <div>
-                <div>
-                <button onClick={this.login}>Login</button>
+                <div className="loginButton">
+                {this.state.authenticated ? this.state.username 
+                : <button onClick={this.login}>Login</button>}
+                
                 </div>
             
                 Hello from the homepage!
