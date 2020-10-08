d57af638131f6bc712e51ff92da481390dccd446
luclemaster <llemaster@email.arizona.edu>
2020-10-08 11:01:02 -0700

    added login

--- imagequiz/src/components/Home.js
+++ imagequiz/src/components/Home.js
@@ -1,14 +1,37 @@
 import React from 'react'
 
 class Home extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state ={
+            showLoginForm: false
+        };
+    }
     login = () => {
-
+        this.setState({
+            showLoginForm: true
+        });
     }
     render() {
+        if (this.state.showLoginForm)
+        {
+            return(
+                <div>
+                    <form>
+                        <lable>Username:</lable>
+                        <input type ="text"></input>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+
+            );
+        }
         return (
             <div>
+                <div>
                 <button onClick={this.login}>Login</button>
+                </div>
+            
                 Hello from the homepage!
             </div>
         );
