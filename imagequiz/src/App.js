f5d1709301a8d112aa0ec0d211679eff727b0367
luclemaster <llemaster@email.arizona.edu>
2020-10-08 10:38:36 -0700

    added login function

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 
+login = () => {
+
+}
 function App() {
   return (
     <div className="App">
+      <button onClick={this.login}>Login</button>
       <Home />
     </div>
   );
